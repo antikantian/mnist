0fd0ab427c5c9e1a2036445a6ac080b20d91ddc4
antikantian <antikantian@users.noreply.github.com>
2016-09-15 15:23:54 -0400

    Small syntax fix.  Opps.

--- src/main/scala/mnist/package.scala
+++ src/main/scala/mnist/package.scala
@@ -6,27 +6,27 @@ package object mnist {
 
   import decoder.idx._
 
-  private[datasets] val trainImgZip = Task.delay {
+  private[mnist] val trainImgZip = Task.delay {
     new GZIPInputStream(getClass.getResourceAsStream("/mnist/train-images-idx3-ubyte.gz"))
   }
 
-  private[datasets] val trainLabelsZip = Task.delay {
+  private[mnist] val trainLabelsZip = Task.delay {
     new GZIPInputStream(getClass.getResourceAsStream("/mnist/train-labels-idx1-ubyte.gz"))
   }
 
-  private[datasets] val testImgZip = Task.delay {
+  private[mnist] val testImgZip = Task.delay {
     new GZIPInputStream(getClass.getResourceAsStream("/mnist/t10k-images-idx3-ubyte.gz"))
   }
 
-  private[datasets] val testLabelsZip = Task.delay {
+  private[mnist] val testLabelsZip = Task.delay {
     new GZIPInputStream(getClass.getResourceAsStream("/mnist/t10k-labels-idx1-ubyte.gz"))
   }
 
-  private[datasets] lazy val imageTraining = idxDecoder(trainImgZip)
-  private[datasets] lazy val labelTraining = idxDecoder(trainLabelsZip)
+  private[mnist] lazy val imageTraining = idxDecoder(trainImgZip)
+  private[mnist] lazy val labelTraining = idxDecoder(trainLabelsZip)
 
-  private[datasets] lazy val imageTesting = idxDecoder(testImgZip)
-  private[datasets] lazy val labelTesting = idxDecoder(testLabelsZip)
+  private[mnist] lazy val imageTesting = idxDecoder(testImgZip)
+  private[mnist] lazy val labelTesting = idxDecoder(testLabelsZip)
 
   sealed trait SetType
   object SetType {
